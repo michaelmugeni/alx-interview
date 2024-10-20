48653780de0885f5acf2ff84c7590674b4ccf1ba
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-20 04:21:00 -0700

    0-minoperations.py updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -34,3 +34,11 @@ def minOperations(n):
     # print('')
     return ops_count
 
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
